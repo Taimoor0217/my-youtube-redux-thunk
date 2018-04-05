e9fb93b62b3d4ecf24eb58c6af2a075b96beb654
Taimoor Khan <taimoor.m.k@live.com>
2018-04-05 11:34:02 +0500

    VideoListEntry testing optimised

--- tests/js/components/VideoListEntry.test.jsx
+++ tests/js/components/VideoListEntry.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { VideoList } from '../../../src/js/components/VideoList';
+import VideoListEntry from '../../../src/js/components/VideoListEntry';
 
 Enzyme.configure({ adapter: new Adapter() });
 
